644db682fcd90f8474ae0bdee71c634043d96c94
Tamim <tamimiskhan@gmail.com>
2021-09-26 12:31:16 +0600

    Getx Dialog .

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: "Sanckbar",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Snackbar"),
+          title: Text("Dialog"),
         ),
         body: Center(
           child: Column(
@@ -20,77 +20,134 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               RaisedButton(
-                child: Text("Show Snackbar"),
                 onPressed: () {
-                  Get.snackbar(
-                    "Snakbar Title", "This will be Snackbar Message",
-                    snackPosition: SnackPosition.BOTTOM,
-                    // titleText: Text("Another Title"),
-                    // messageText: Text(
-                    //   "Another message",
-                    //   style: TextStyle(color: Colors.white),
-                    // ),
-                    colorText: Colors.yellowAccent,
-                    backgroundColor: Colors.black,
-                    borderRadius: 30,
-                    margin: EdgeInsets.all(10),
-                    // maxWidth: 200,
-                    animationDuration: Duration(milliseconds: 3000),
-                    backgroundGradient: LinearGradient(
-                      colors: [Colors.green, Colors.red],
+                  //  Get.defaultDialog();
+                  Get.defaultDialog(
+                    title: "Dialog Title",
+                    middleText: "This is middle text",
+                    middleTextStyle: TextStyle(fontSize: 20),
+                    backgroundColor: Colors.purple,
+                    radius: 50,
+                    //To Custom the middle Text
+                    content: Row(
+                      children: [
+                        CircularProgressIndicator(),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Text("Data Loading"),
+                        ),
+                      ],
                     ),
-                    borderColor: Colors.purple,
-                    borderWidth: 3,
-                    boxShadows: [
-                      BoxShadow(
-                          color: Colors.yellow,
-                          offset: Offset(30, 50),
-                          spreadRadius: 20,
-                          blurRadius: 8),
-                    ],
-                    isDismissible: true,
-                    dismissDirection: SnackDismissDirection.HORIZONTAL,
-                    forwardAnimationCurve: Curves.bounceInOut,
-                    duration: Duration(milliseconds: 8000),
-                    icon: Icon(
-                      Icons.send,
-                      color: Colors.white,
+
+                    //Default cancle and confirm action
+                    textCancel: "Cancle",
+                    cancelTextColor: Colors.white,
+                    textConfirm: "Confirm",
+                    confirmTextColor: Colors.white,
+                    onCancel: () {},
+                    onConfirm: () {},
+                    buttonColor: Colors.green,
+                    cancel: Text(
+                      "Cancels",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    shouldIconPulse: false,
-                    leftBarIndicatorColor: Colors.white,
-                    mainButton: TextButton(
-                      child: Text(
-                        "Retry",
-                        style: TextStyle(color: Colors.white),
+                    confirm: Text(
+                      "Confirms",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    actions: [
+                      RaisedButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: Text("Action-2"),
                       ),
-                      onPressed: () {},
-                    ),
-                    onTap: (val) {
-                      print("Retry clicked");
-                    },
-                    overlayBlur: 5,
-                    //overlayColor: Colors.grey
-                    padding: EdgeInsets.all(50),
-                    showProgressIndicator: true,
-                    progressIndicatorBackgroundColor: Colors.deepOrange,
-                    progressIndicatorValueColor:
-                        AlwaysStoppedAnimation<Color>(Colors.white),
-                    reverseAnimationCurve: Curves.bounceInOut,
-                    snackbarStatus: (val) {
-                      print(val);
-                    },
-                    // userInputForm: Form(
-                    //   child: Row(
-                    //     children: [
-                    //       Expanded(
-                    //         child: TextField(),
-                    //       )
-                    //     ],
-                    //   ),
-                    // ),
+                      RaisedButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: Text("Action-1"),
+                      ),
+                    ],
+                    barrierDismissible: false,
                   );
                 },
+                child: Text("Show Dialog"),
               )
+              // RaisedButton(
+              //   child: Text("Show Snackbar"),
+              //   onPressed: () {
+              //     Get.snackbar(
+              //       "Snakbar Title", "This will be Snackbar Message",
+              //       snackPosition: SnackPosition.BOTTOM,
+              //       // titleText: Text("Another Title"),
+              //       // messageText: Text(
+              //       //   "Another message",
+              //       //   style: TextStyle(color: Colors.white),
+              //       // ),
+              //       colorText: Colors.yellowAccent,
+              //       backgroundColor: Colors.black,
+              //       borderRadius: 30,
+              //       margin: EdgeInsets.all(10),
+              //       // maxWidth: 200,
+              //       animationDuration: Duration(milliseconds: 3000),
+              //       backgroundGradient: LinearGradient(
+              //         colors: [Colors.green, Colors.red],
+              //       ),
+              //       borderColor: Colors.purple,
+              //       borderWidth: 3,
+              //       boxShadows: [
+              //         BoxShadow(
+              //             color: Colors.yellow,
+              //             offset: Offset(30, 50),
+              //             spreadRadius: 20,
+              //             blurRadius: 8),
+              //       ],
+              //       isDismissible: true,
+              //       dismissDirection: SnackDismissDirection.HORIZONTAL,
+              //       forwardAnimationCurve: Curves.bounceInOut,
+              //       duration: Duration(milliseconds: 8000),
+              //       icon: Icon(
+              //         Icons.send,
+              //         color: Colors.white,
+              //       ),
+              //       shouldIconPulse: false,
+              //       leftBarIndicatorColor: Colors.white,
+              //       mainButton: TextButton(
+              //         child: Text(
+              //           "Retry",
+              //           style: TextStyle(color: Colors.white),
+              //         ),
+              //         onPressed: () {},
+              //       ),
+              //       onTap: (val) {
+              //         print("Retry clicked");
+              //       },
+              //       overlayBlur: 5,
+              //       //overlayColor: Colors.grey
+              //       padding: EdgeInsets.all(50),
+              //       showProgressIndicator: true,
+              //       progressIndicatorBackgroundColor: Colors.deepOrange,
+              //       progressIndicatorValueColor:
+              //           AlwaysStoppedAnimation<Color>(Colors.white),
+              //       reverseAnimationCurve: Curves.bounceInOut,
+              //       snackbarStatus: (val) {
+              //         print(val);
+              //       },
+              //       // userInputForm: Form(
+              //       //   child: Row(
+              //       //     children: [
+              //       //       Expanded(
+              //       //         child: TextField(),
+              //       //       )
+              //       //     ],
+              //       //   ),
+              //       // ),
+              //     );
+              //   },
+              // ),
             ],
           ),
         ),
