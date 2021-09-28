5e5071a4fff7a3661def388eb642e8ed8b78297c
Tamim <tamimiskhan@gmail.com>
2021-09-28 11:17:51 +0600

    Getx Getx controller.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'student.dart';
+import 'package:getx_flutter/my_controller.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var count = 0.obs;
 
-  void increment() {
-    count++;
-  }
-
-  // var student=Student();
-
-  final student = Student(name: 'tom', age: 25).obs;
+  MyController myController = Get.put(MyController());
 
   @override
   Widget build(BuildContext context) {
@@ -32,20 +24,15 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Obx(
-                    () =>
-                    Text(
-                      "Name is ${student.value.name}",
-                      style: TextStyle(fontSize: 25),
-                    ),
+                () => Text(
+                  "Name is ${myController.student.value.name}",
+                  style: TextStyle(fontSize: 25),
+                ),
               ),
               RaisedButton(
                 child: Text("Upper"),
                 onPressed: () {
-                  increment();
-
-                  student.update((student) {
-                    student!.name = student?.name.toString().toUpperCase();
-                  });
+                  myController.convertToUpperCase();
 
                   //  student.name.value = student.name.value.toUpperCase();
                 },
