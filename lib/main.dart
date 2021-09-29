fae19ac42b3e7f08f87c1def8cf12bb291642544
Tamim <tamimiskhan@gmail.com>
2021-09-29 13:16:56 +0600

    Getx GetX Service.

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_flutter/Messages.dart';
 import 'package:getx_flutter/my_controller.dart';
+import 'package:getx_flutter/service.dart';
 
-void main() {
+Future<void> main() async {
+  await initServices();
   runApp(MyApp());
 }
 
+Future<void> initServices() async {
+  print('starting service ...');
+  await Get.putAsync<Service>(() async => await Service());
+  print('All service started..');
+}
+
 class MyApp extends StatelessWidget {
-//If init property us not used Getx<Type of controller > then
-//create the instance of controller as follow
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
-
-
-// MyController myController = Get.put(MyController(), permanent: true);
-
-
-  //  Get.lazyPut(() => MyController(),tag: 'instance2',fenix: true);
-    
-   // Get.create<MyController>(() => MyController());
-
-    Get.putAsync<MyController>(() async=> await MyController());
+    Get.putAsync<MyController>(() async => await MyController());
     return GetMaterialApp(
-      title: "Dependancy Injection",
+      title: "GetX Service",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Dependancy Injection"),
+          title: Text("GetX Service"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-
               RaisedButton(
                 onPressed: () {
-               // Get.find<MyController>(tag: 'instance1');
-                Get.find<MyController>();
+                  // Get.find<MyController>(tag: 'instance1');
+                  // Get.find<MyController>();
 
-                Get.find<MyController>().incrementCounter();
+                  Get.find<Service>().incrementCounter();
                 },
-                child: Text("Clcik me"),
+                child: Text("Increment"),
               ),
-
             ],
           ),
         ),
